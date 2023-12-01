f6c4ceab3615e75d076591d2b5e8f3502dd3e02e
furai <furai911@gmail.com>
2023-11-30 21:15:20 -0800

    queue name added

--- broker.go
+++ broker.go
@@ -5,15 +5,15 @@ import (
 )
 
 type BrokerIncrementObserver interface {
-	IncBrokerMessagesReads(result string)
-	IncBrokerMessagesWrites(result string)
-	NewBrokerWriteTimer() *prometheus.Timer
+	IncBrokerMessagesReads(queue, result string)
+	IncBrokerMessagesWrites(queue, result string)
+	NewBrokerWriteTimer(queue string) *prometheus.Timer
 }
 
 type BrokerMetrics struct {
 	brokerMessagesReadsTotal  *prometheus.CounterVec
 	brokerMessagesWritesTotal *prometheus.CounterVec
-	brokerWriteDuration       prometheus.Histogram
+	brokerWriteDuration       *prometheus.HistogramVec
 }
 
 var _ BrokerIncrementObserver = &BrokerMetrics{}
@@ -24,33 +24,33 @@ func NewBrokerMetrics() *BrokerMetrics {
 	metrics.brokerMessagesReadsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "broker_messages_reads_total",
 		Help: "Count of messages read from queues, attempts and failures separately",
-	}, []string{"result"})
+	}, []string{"queue", "result"})
 	prometheus.MustRegister(metrics.brokerMessagesReadsTotal)
 
 	metrics.brokerMessagesWritesTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "broker_messages_writes_total",
 		Help: "Count of messages written to queues, attempts and failures separately",
-	}, []string{"result"})
+	}, []string{"queue", "result"})
 	prometheus.MustRegister(metrics.brokerMessagesWritesTotal)
 
-	metrics.brokerWriteDuration = prometheus.NewHistogram(prometheus.HistogramOpts{
+	metrics.brokerWriteDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "broker_write_duration",
 		Help:    "A Histogram of the write operation duration in seconds",
 		Buckets: CustomBuckets,
-	})
+	}, []string{"queue"})
 	prometheus.MustRegister(metrics.brokerWriteDuration)
 
 	return metrics
 }
 
-func (m *BrokerMetrics) IncBrokerMessagesReads(result string) {
-	m.brokerMessagesReadsTotal.WithLabelValues(result).Inc()
+func (m *BrokerMetrics) IncBrokerMessagesReads(queue, result string) {
+	m.brokerMessagesReadsTotal.WithLabelValues(queue, result).Inc()
 }
 
-func (m *BrokerMetrics) IncBrokerMessagesWrites(result string) {
-	m.brokerMessagesWritesTotal.WithLabelValues(result).Inc()
+func (m *BrokerMetrics) IncBrokerMessagesWrites(queue, result string) {
+	m.brokerMessagesWritesTotal.WithLabelValues(queue, result).Inc()
 }
 
-func (m *BrokerMetrics) NewBrokerWriteTimer() *prometheus.Timer {
-	return prometheus.NewTimer(m.brokerWriteDuration)
+func (m *BrokerMetrics) NewBrokerWriteTimer(queue string) *prometheus.Timer {
+	return prometheus.NewTimer(m.brokerWriteDuration.WithLabelValues(queue))
 }
